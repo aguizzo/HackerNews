a2cbf03cd15b9acdcde8409335ed3197f0341c60
Andres <andres.leandro.guizzo@estudiantat.upc.edu>
2021-10-21 17:23:22 +0200

    algo

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   resources :submissions
+    get '/submit', to: 'submissions#new', as: 'submit'
   resources :users
-  get 'home/index'
+    get '/login', to: 'users#new', as: 'login'
+
+  get "/news", to:'home#index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
