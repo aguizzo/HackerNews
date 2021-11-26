98cce195ee665fcccec7f2d1bad1f07289e11ddb
AdriaGarcia <thekronsonder@gmail.com>
2021-11-26 17:01:07 +0100

    canvi users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,37 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: %i[ show edit update destroy ]
-    def new
-        @user = User.new
-    end
+  before_action :set_user, only: %i[ show edit update destroy ]
+  def new
+    @user = User.new
+  end
 
-    def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
-        else
-            render :new
-        end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
     end
+  end
 
-    def show
-        @user = User.find(params[:id])
-    end
-      # GET /users or /users.json
+  # GET /users or /users.json
   def index
     @users = User.all
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @users}
+    end
   end
 
   # GET /users/1 or /users/1.json
   def show
+    @user = User.find(params[:id])
+    respond_to do |format|
+      format.html {render :show}
+      format.json {render json: @user}
+    end
   end
-
+  
   # GET /users/new
   def new
     @user = User.new
